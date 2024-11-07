059216046a5183c5730c6679fdc18cc3a4065889
Isaac-Valenzuela <isaacamaya941@gmail.com>
2024-11-07 08:53:27 -0500

    cambios 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
